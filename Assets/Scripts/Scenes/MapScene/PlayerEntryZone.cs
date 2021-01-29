3337fa189d8be09fb2586521e2f4978e7ca39192
DESKTOP-OO112VQ\fild7 <fild.7523@gmail.com>
2021-01-29 14:36:22 +0900

    マップシーンにて、仮でマップチップを挿入 プレイヤーが一マス単位で歩けるように 人の実装

---
+++ Assets/Scripts/Scenes/MapScene/PlayerEntryZone.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntryZone : MonoBehaviour
+{
+    [SerializeField] public Vector2 Up = Vector2.zero;
+    [SerializeField] public Vector2 Down = Vector2.zero;
+    [SerializeField] public Vector2 Right = Vector2.zero;
+    [SerializeField] public Vector2 Left = Vector2.zero;
+
+    public HumanMoveMap _collision_object = null; 
+
+    public bool is_collider { get; set; } = false;
+
+    public void SetEntryZonePosUp() { transform.localPosition = Up; }
+    public void SetEntryZonePosDown() { transform.localPosition = Down; }
+    public void SetEntryZonePosRight() { transform.localPosition = Right; }
+    public void SetEntryZonePosLeft() { transform.localPosition = Left; }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Human")
+        {
+            is_collider = true;
+            _collision_object = collision.gameObject.GetComponent<HumanMoveMap>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Human")
+        {
+            is_collider = false;
+            _collision_object = null;
+        }
+    }
+}
