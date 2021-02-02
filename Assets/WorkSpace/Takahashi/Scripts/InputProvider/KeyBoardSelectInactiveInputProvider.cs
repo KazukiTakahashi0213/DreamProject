198afd8fa493704ce691f02313836959fa3a51d2
KazukiTakahashi0213 <57526865+KazukiTakahashi0213@users.noreply.github.com>
2021-02-02 17:47:24 +0900

    微調整

---
+++ Assets/WorkSpace/Takahashi/Scripts/InputProvider/KeyBoardSelectInactiveInputProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBoardSelectInactiveInputProvider : IInputProvider {
+	public bool UpSelect() {
+		return false;
+	}
+	public bool DownSelect() {
+		return false;
+	}
+	public bool RightSelect() {
+		return false;
+	}
+	public bool LeftSelect() {
+		return false;
+	}
+	public bool SelectEnter() {
+		return Input.GetKeyDown(KeyCode.Z);
+	}
+	public bool SelectBack() {
+		return Input.GetKeyDown(KeyCode.X);
+	}
+	public bool SelectNovelWindowActive() {
+		return Input.GetKeyDown(KeyCode.W);
+	}
+}
